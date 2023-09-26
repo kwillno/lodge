f862413bc59178aad396150e9dec782bd999fc87
Kristian Willassen <kristian@kwill.no>
2023-09-26 14:04:18 +0200

    Some preliminary fixes for future problems

--- lodge.py
+++ lodge.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from jakt import jakt, timeslot, JaktReport, JaktError, JaktActiveError, JaktNotActiveError
 
 app = Flask(__name__)
@@ -60,6 +60,7 @@ def api_index():
 
     return response
 
+@app.route('/api/start/<project>/<tags>')
 @app.route('/api/start/<project>/<tags>/')
 def api_start(project, tags):
     """
@@ -82,6 +83,7 @@ def api_start(project, tags):
 
 
 @app.route("/api/status")
+@app.route("/api/status/")
 def api_status():
     try:
         response = jkt.status()
@@ -110,7 +112,20 @@ def api_stop():
         }
     return response
 
+@app.route("/api/add", methods=["PUT", "POST"])
+@app.route("/api/add/", methods=["PUT", "POST"])
+def api_add():
+
+    try:
+        ts = timeslot.from_json(request.json)
+        response = jkt.add(ts)
+        return response
+    except KeyError as e:
+        return {"error": 1, "msg": f"Invalid timeslot format on key {e}"}
+
+
 @app.route("/api/ls")
+@app.route("/api/ls/")
 def api_ls():
     timeslots = jkt.getTimeslots()
 
@@ -121,10 +136,36 @@ def api_ls():
 
     return timeslots
 
+@app.route("/api/ls/<id>")
+@app.route("/api/ls/<id>/")
+def api_ls_index(id):
+    timeslots = jkt.getTimeslots()
+
+    for i in range(len(timeslots)):
+        if timeslots[i].id == id:
+            return timeslots[i].toDict()
+
+    return {}
+
 @app.route("/api/report")
-def api_report():
-    rpt = jkt.report()
+@app.route("/api/report/")
+@app.route("/api/report/<project>")
+@app.route("/api/report/<project>/")
+@app.route("/api/report/<project>/<tags>")
+@app.route("/api/report/<project>/<tags>/")
+def api_report(project = None, tags = None):
 
-    project_report = rpt.getProjectReport()
+    if project:
+        rpt = jkt.report()
+        project_report = rpt.getTagReport(project=project)
 
-    return project_report
+        return project_report
+
+    else:
+        rpt = jkt.report()
+
+        project_report = rpt.getProjectReport()
+        for i in range(len(project_report)):
+            project_report[i]["tags"] = rpt.getTagReport(project=project_report[i]["project"])
+
+        return project_report
